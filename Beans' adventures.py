5e5b29d65a92aa7ce7759c682f5af252da89e2f0
Consideratebanjo <aleppleton@gmail.com>
2024-10-10 20:56:02 +0100

    Update After some tinkering, now an entity can perform skillchecks. Thrilling stuff. Now, only file handling to go, and it'll be working -- albeit only the barest, most naked skeleton you'll ever see, but every necromancer gotta start somewhere.

--- Beans' adventures.py
+++ Beans' adventures.py
@@ -1,12 +1,16 @@
-#importing dependancies
+#========= importing dependancies
 
 import random as rand  #allows dice rolls
 
+#========= defining subroutines
+#rolls a die of author-declared faces
+def roll(die):
+    return(rand.randint(die))
 
-#class for an entity/thing, player, monster, etc
-class entity:
+#class for an entity/thing; player, monster, etc
+class Entity:
     
-    #creates an entity with average statistics -- skillcheck modifiers will be 0 by default
+    #creates an entity with (by default) average statistics -- skillcheck modifiers will be 0 by default
     def __init__(self, name, strength=10, dexterity=10, constitution=10, intelligence=10, wisdom=10, charisma=10):
         # a name and the 6 core stats used in most dnd-esque systems -- this will hopefully change in future, allowing authors to declare their own stats
         self.name = name
@@ -20,19 +24,19 @@ class entity:
     # Takes an entity's statistic and a "difficulty class", allowing for a declared modifier, rolls a die (20-sided by default, but changeable) and allows for advantage/disadvantage
     def skillcheck(stat,dc,modifier=0,die=20,adv=0):
         modifier += (stat-10) // 2  #Converts an entity's stat into a modifier, and adds it to the declared modifier
-        roll = rand.randint(0,die) #rolls the first die
+        dieroll = roll(die) #rolls the first die
 
         if (adv>0): #if entity makes the check with x advantage,
             for i in adv: #per x,
-                roll2 = rand.randint(0,die) #roll another die,
-                if (roll2>roll):  #compare the two rolls, and take whichever die is higher
-                    roll = roll2
+                dieroll2 = roll(die) #roll another die,
+                if (dieroll2>dieroll):  #compare the two rolls, and take whichever die is higher
+                    dieroll = dieroll2
                 i+1
         elif (adv<0): # if entity makes check with disadvantage,
             for i in (adv*-1): #per disadvantage,
-                roll2 = rand.randint(0,die) #roll another die
-            if (roll2<roll): # compare the two rolls, and take whichever die is higher
-                    roll = roll2
+                dieroll2 = roll(die) #roll another die
+            if (dieroll2<dieroll): # compare the two rolls, and take whichever die is higher
+                    dieroll = dieroll2
                 i+1
         else: #if no advantage or disadvantage,
             pass #continue without rolling any additional dice
@@ -44,5 +48,8 @@ class entity:
             return("Success")
         else:
             return("Failure")
-                
-#this is a test.
+
+#========== main code body/process
+
+character = Entity('Beans')
+print(Beans.skillcheck(12,2))
