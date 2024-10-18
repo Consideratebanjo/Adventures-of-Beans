4b89a156591af7080f0346469fbdbd69c80e1713
ConsiderateBanjo <aleppleton@gmail.com>
2024-10-18 19:29:26 +0100

    Release 1.0: basic. Program can navigate around pages in the "Story" folder.

--- Beans' adventures.py
+++ Beans' adventures.py
@@ -1,105 +1,50 @@
-#========= importing dependancies
-
-import random as rand  #allows dice rolls
-
-
-#========= defining subroutines & classes
-
-def roll(die):  #rolls a die of author-declared faces
-    return(rand.randint(1,die))
-
-class Entity:  #class for an entity/thing; player, monster, etc
-
-    
-    #creates an entity with (by default) average statistics -- skillcheck modifiers will be 0 by default
-    def __init__(self, name, strength=10, dexterity=10, constitution=10, intelligence=10, wisdom=10, charisma=10):
-        # a name and the 6 core stats used in most dnd-esque systems -- this will hopefully change in future, allowing authors to declare their own stats
-        self.name = name
-        self.str = strength
-        self.dex = dexterity
-        self.con = constitution
-        self.int = intelligence
-        self.wis = wisdom
-        self.cha = charisma
-
-    # Takes an entity's statistic and a "difficulty class", allowing for a declared modifier, rolls a die (20-sided by default, but changeable) and allows for advantage/disadvantage
-    def skillcheck(self,stat,dc,modifier=0,die=20,adv=0):
-        modifier += (stat-10) // 2  #Converts an entity's stat into a modifier, and adds it to the declared modifier
-        dieroll = roll(die) #rolls the first die
-
-        if (adv>0): #if entity makes the check with x advantage,
-            for i in adv: #per x,
-                dieroll2 = roll(die) #roll another die,
-                if (dieroll2>dieroll):  #compare the two rolls, and take whichever die is higher
-                    dieroll = dieroll2
-                i+1
-        elif (adv<0): # if entity makes check with disadvantage,
-            for i in (adv*-1): #per disadvantage,
-                dieroll2 = roll(die) #roll another die
-            if (dieroll2<dieroll): # compare the two rolls, and take whichever die is higher
-                    dieroll = dieroll2
-
-        else: #if no advantage or disadvantage,
-            pass #continue without rolling any additional dice
-
-        score = (dieroll+modifier) #adds modifier to the used die roll into a score
-
-        #if score meets or exceeds the difficulty, return "success" to indicate the entity passes the skill check. Otherwise, indicates the entity fails the check by returning "Failure"
-        if (score >= dc): 
-            return("Success")
-        else:
-            return("Failure")
-
-
-#========== Placeholder or WIP testing
-
 next = "Start" #placeholder variable
 path = f"Story/{next}.txt"
 file = open(path)
 Choices = {}
 next = "Start" #next page to open
 
-while True:
-    path = f"Story/{next}.txt"
-    count = 0
-    Choices.clear()
+while True: #until user closes/kills the program,
+    path = f"Story/{next}.txt" #sets the next file to be opened to an easier-to-refer-to variable
+    count = 0 #resets a counting variable to 0
+    Choices.clear() #clears the log of choicecs pulled from the previous document, if applicable
 
 
-    with open(path) as file:
-        reading = True
-        story = True
-        choosing = False
+    with open(path) as file: #open file -- this form automatically closes the document when indented code block is completed
+        reading = True #variable used to remember when we are done with reading, and must present the user with a choice
+        story = True #variable used to remember we are still reading story and outputting to the user
+        choosing = False #variable used to remember when we are documenting choices to present to the user
 
-        while (reading == True):
+        while (reading == True): #while the program knows it's still reading the document,
 
-            for line in file:
-                line = line.removesuffix("\n")
+            for line in file:  #Each line in the file is iterated through,
+                line = line.removesuffix("\n")  #the line is cleared of it's "enter key" stroke at the end, so output is not double-spaced on lines
 
-                if (story == True):
-                    if (line == "*ENDSTORY*"):
-                        print("Do you:\n")
-                        story = False
-                        choosing = True
-                    else:
-                        print(line)
+                if (story == True): #while the program is still reading story to the user,
+                    if (line == "*ENDSTORY*"): #if it hits the end of the story,
+                        print("Do you:\n") #introduces choices,
+                        story = False #stops outputting text and begins documenting choices
+                        choosing = True 
+                    else:   #if the line is not the endline,
+                        print(line) #outputs it
 
-                elif (choosing == True):
+                elif (choosing == True): #if we're choosing, continues reading line-by-line
 
-                    if (line == "*ENDCHOICE*"):
-                        print(Choices)
-                        reading = False
+                    if (line == "*ENDCHOICE*"): #if the line is the end of the choices seciton,
+                        print(Choices) #presents the choises to the user
+                        reading = False #signals that choices have been presented, and signals to close the page
 
-                    else:
-                        line = line.replace(" ", "").split(":")
-                        if (len(line) == 2 ):
-                            Choices[line[0]] = line[1]
+                    else: #if the line is not *ENDCHOICES*, 
+                        line = line.replace(" ", "").split(":") #removes any spaces, and splits it into two items, around (not including) the colon
+                        if (len(line) == 2 ): #if there are indeed two items (ie. line was not blank),
+                            Choices[line[0]] = line[1] #adds the line's two items to a dictionary
                         
                     
-    nextchoice = str(input())
-    for i in Choices:
-        if (i == nextchoice):
-            next = Choices[i]
-    print("\n=============================\n")
+    nextchoice = str(input())  #takes an input and casts it as a string, so it can be compared to the dictionary of choices held
+    for i in Choices: #for each entry in the choices dictionary,
+        if (i == nextchoice): #if the entry corresponds to the user input,
+            next = Choices[i] #sets the corresponding document name as the next page to be opened
+    print("\n=============================\n") #once a choice has been made, prints an enter, a line of = symbols, and another enter -- to clear up the flow and make it more obviousn where the page turns
                 
                     
 
